28658ac88f300d7d993ebcada103248318786f35
infinitom <deva.nadesan@infinitom.com>
2014-10-21 14:53:50 -0400

    First iteration - Setup specified range and display sequentially on screen

--- exercise4.rb
+++ exercise4.rb
@@ -2,3 +2,13 @@
 # Exercise 4
 # Bitmaker Labs version of FizzBuzz
 #
+
+
+# Specify range
+one_to_hundred = (1..100)
+
+
+# Display one to hundred sequentially
+one_to_hundred.each do |number|
+  print number
+end
