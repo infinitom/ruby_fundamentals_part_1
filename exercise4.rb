53bacf3bd19e13d5ba3107877f6fd49fe2ca72a6
infinitom <deva.nadesan@infinitom.com>
2014-10-21 21:49:31 -0400

    Using case dure to multiple condtions and a default condition. Now with fancy colorized output!

--- exercise4.rb
+++ exercise4.rb
@@ -3,6 +3,9 @@
 # Bitmaker Labs version of FizzBuzz
 #
 
+#for fancy colours n stuff
+require 'colorize'
+
 
 # Specify range
 one_to_hundred = (1..100)
@@ -10,13 +13,22 @@ one_to_hundred = (1..100)
 
 # Display one to hundred sequentially
 one_to_hundred.each do |number|
-  if (number%3==0 && number%5==0)
-    print "BitMaker\s"
-  elsif (number%3==0)
-    print "Bit\s"
-  else
-    print "#{number}\s"
+
+# Using case statement instead of "if"
+# due to multiple 
+# conditions: 3, 5, 3&5, and a default, i.e., no condition
+
+  case 
+  	when (number%3==0 && number%5==0)
+  		print "\nBitMaker\s\s".blue
+  	when (number%3==0)
+  		print "Bit\s".red
+  	when (number%5==0)
+  		print "Maker\s\s".green
+  	else
+  		print "#{number}\s\s".yellow
   end
+
 end
 
 puts "\n"
